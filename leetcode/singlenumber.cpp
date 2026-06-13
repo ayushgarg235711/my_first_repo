943dc11b487ab2f3f465a384ce38878747579336
ayushgarg235711 <ayushgarg235711@gmail.com>
2026-06-13 23:59:52 +0530

    add single number solution

---
+++ leetcode/singlenumber.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for (int val : nums) {
+            ans = ans ^ val;
+        }
+        return ans;
+    }
+};
